3885a101c7505918c68e325c4c3e6f5679b93f04
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-07-04 16:39:54 +0300

    Lockboxes problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/bin/env python3
+"""
+Module for lockboxes method.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Method that determines if all the boxes can be opened.
+    """
+    if len(boxes) == 0:
+        return False
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    if len(keys) == len(boxes):
+        return True
+    return False
